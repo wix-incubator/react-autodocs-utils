622ebe31646d537ba5bba3a6104cd8b74e1061e6
argshook <argshook@gmail.com>
2018-01-25 17:36:35 +0200

    test(read-file)

---
+++ src/fs/read-file.test.js
@@ -0,0 +1,24 @@
+/* global describe it expect jest */
+
+jest.mock('fs');
+const fs = require('fs');
+
+const readFile = require('./read-file');
+
+
+describe('readFile', () => {
+  describe('given existing path', () => {
+    it('should resolve with file content', () => {
+      const content = 'hello file content';
+      fs.__setFile('test.file')(content);
+
+      return expect(readFile('test.file')).resolves.toEqual(content);
+    });
+  });
+
+  describe('given non existing path', () => {
+    it('should reject with error', () =>
+      expect(readFile('you-dont-exist')).rejects.toBeDefined()
+    );
+  });
+});
