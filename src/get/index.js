81f80064cfbfa79882a3fb2b827c1e4fb4e7073a
argshook <argshook@gmail.com>
2018-01-29 16:57:55 +0200

    feat(get): add utility fn to retrieve nested properties

---
+++ src/get/index.js
@@ -0,0 +1,32 @@
+/**
+  * helper function to retrieve deeply nested properties
+  * cause i don't need lodash, it's easy enough to implement
+  *
+  * given
+  *
+  * object = {
+  *   nested: {
+  *     deeply: {
+  *       array: [ 'first', { second: 'hello' } ]
+  *     }
+  *   }
+  * }
+  *
+  * get(object)('nested.deeply.array.1.second') // <-- 'hello'
+  */
+
+
+// get -> Object a -> String -> a
+const get = object => path =>
+  path
+    .split('.')
+    .reduce(
+      (resultObject, pathPart) =>
+        resultObject && resultObject[pathPart]
+          ? resultObject[pathPart]
+          : null,
+      object
+    );
+
+
+module.exports = get;
---
+++ src/get/index.test.js
@@ -0,0 +1,37 @@
+/* global describe it expect */
+
+const get = require('./');
+
+describe('get()', () => {
+  it('should return function when called', () => {
+    expect(typeof get()).toBe('function');
+  });
+
+  describe('given object path "descriptor"', () => {
+    it('should return value', () => {
+      const object = {
+        nested: {
+          deeply: {
+            array: [ 'first', { second: 'hello' } ]
+          }
+        }
+      };
+
+      const path = 'nested.deeply.array.1.second';
+      expect(get(object)(path)).toEqual('hello');
+    });
+  });
+
+  describe('given path "descriptor" pointing to non existing property', () => {
+    it('should return null', () => {
+      const object = {
+        nested: {
+          deeply: 'hello'
+        }
+      };
+
+      const path = 'are.you.still.there.?';
+      expect(get(object)(path)).toEqual(null);
+    });
+  });
+});
