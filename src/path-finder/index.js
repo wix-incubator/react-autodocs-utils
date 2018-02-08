d45c37aa4f6a85dc6f77f1f6808f10e6f8957fc0
argshook <argshook@gmail.com>
2018-02-08 12:53:40 +0200

    feat(pathFinder): resolve component path from import
    so that `componentPath` is not necessary

--- src/path-finder/index.js
+++ src/path-finder/index.js
@@ -1,19 +1,48 @@
 /* global Promise */
 
 const recastVisit = require('../parser/recast-visit');
+const get = require('../get');
 
+const getPropertyValue = path => propertyName =>
+  get(path)('node.declaration.properties')
+    .find(({ key: { name } }) => name === propertyName);
 
-const pathFinder = (source = '') =>
-  new Promise((resolve) =>
-    recastVisit(source)({
+const pathFinder = (source = '') => {
+  const visit = recastVisit(source);
+
+  return new Promise((resolve, reject) => {
+    visit({
       visitExportDefaultDeclaration: function(path) {
-        const componentPath = path.node.declaration.properties
-          .find(({ key: { name } }) => name === 'componentPath');
+        const componentPath = get(getPropertyValue(path)('componentPath'))('value.value');
 
-        resolve(componentPath.value.value);
+        if (componentPath) {
+          resolve(componentPath);
+          return false;
+        } else {
+          const componentReference = get(getPropertyValue(path)('component'))('value.name');
+
+          visit({
+            visitImportDeclaration: function(path) {
+              const componentPath = get(path)('node.specifiers')
+                .find(({ local: { name } }) => name === componentReference);
+
+              if (componentPath) {
+                resolve(get(path)('node.source.value'));
+                return false;
+              }
+
+              this.traverse(path);
+            }
+          });
+
+          resolve(componentReference);
+        }
       }
-    })
-  );
+    });
+
+    reject(new Error('ERROR: Unable to resolve path to component'));
+  });
+};
 
 
 module.exports = pathFinder;
--- src/path-finder/index.test.js
+++ src/path-finder/index.test.js
@@ -3,16 +3,54 @@
 const pathFinder = require('./');
 
 describe('pathFinder()', () => {
-  it('should return a promise', () => {
-    expect(pathFinder().then).toBeDefined();
-  });
-
-  describe('given valid source', () => {
+  describe('given `componentPath`', () => {
     it('should resolve promise with value of `componentPath`', () => {
-      const expectation = 'hello'.repeat(Math.random() * 19);
+      const expectation = '.' + 'hello'.repeat(Math.random() * 19);
       const source = `export default { componentPath: '${expectation}' }`;
 
       return expect(pathFinder(source)).resolves.toEqual(expectation);
+    });
+  });
+
+  describe('given `component` without `componentPath`', () => {
+    describe('when `component` is imported', () => {
+      const path = './path';
+      const sourceTestCases = [
+        `import Component from '${path}';
+        export default {
+          component: Component
+        }`,
+
+        `import * as Component from '${path}'
+        export default {
+          component: Component
+        }`,
+
+        `import {Component} from '${path}'
+        export default {
+          component: Component
+        }`,
+
+        `import {Component, Something, Different} from '${path}'
+        export default {
+          component: Component
+        }`,
+
+        `import {Component as ComponentAlias} from '${path}'
+        export default {
+          component: ComponentAlias
+        }`,
+
+        `import defaultExport, {Component as ComponentAlias} from '${path}'
+        export default {
+          component: ComponentAlias
+        }`
+      ];
+
+      sourceTestCases.map(source =>
+        it('should resolve promise with path to component', () =>
+          expect(pathFinder(source)).resolves.toEqual(path)
+        ));
     });
   });
 });
