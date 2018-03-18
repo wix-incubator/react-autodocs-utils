0f3678f26bd9ee3b07e939729eec02cf4d06ffd3
argshook <argshook@gmail.com>
2018-03-19 00:05:16 +0200

    feat(gatherAll): work with file paths too

--- src/gather-all/index.js
+++ src/gather-all/index.js
@@ -1,6 +1,7 @@
 /* global Promise */
 
 const pathJoin = require('path').join;
+const dirname = require('../dirname');
 const readFolder = require('../read-folder');
 const readFile = require('../read-file');
 const metadataParser = require('../metadata-parser');
@@ -14,13 +15,11 @@ const containsFile = files => name => {
     : Promise.reject();
 };
 
-
 // isPath : String -> Promise
 const isPath = path =>
   path
     ? Promise.resolve(path)
     : Promise.reject('Error: gatherAll is missing required `path` argument');
-
 
 // error : String -> Promise
 const error = message =>
@@ -29,19 +28,26 @@ const error = message =>
 
 const gatherAll = path =>
   isPath(path)
-    .then(readFolder)
+    .then(metadataParser)
 
-    .then(files => {
-      const metadata = metadataParser(path)
-        .catch(e =>
-          error(`Unable to parse component in path "${path}", reason: ${e}`)
-        );
+    .catch(e =>
+      error(`Unable to parse component in path "${path}", reason: ${e}`)
+    )
 
+    .then(metadata =>
+      Promise.all([
+        Promise.resolve(metadata),
+        readFolder(path)
+      ])
+    )
+
+    .then(([metadata, files]) => {
       const readMarkdown = markdownPath =>
         containsFile(files)(markdownPath)
-          .then(file => readFile(pathJoin(path, file)))
+          .then(file => readFile(pathJoin(dirname(path), file)))
           .then(({source}) => source)
           .catch(() => Promise.resolve(''));
+
 
       const readme = readMarkdown('readme.md');
       const readmeAccessibility = readMarkdown('readme.accessibility.md');
