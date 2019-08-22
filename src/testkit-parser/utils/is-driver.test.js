96e400218484156c96b5c6d855bc9f51e7fd2e62
argshook <argshook@gmail.com>
2019-08-22 17:05:21 +0300

    test(is-driver): more test cases

--- src/testkit-parser/utils/is-driver.test.js
+++ src/testkit-parser/utils/is-driver.test.js
@@ -1,8 +1,18 @@
 const isDriver = require('./is-driver');
 
 describe('isDriver', () => {
-  [['file.driver.js', true], ['file.private.driver.js', false]].map(([assertion, expecation]) => {
-    it('given ${assertion} it should return ${expecation}', () => {
+  [
+    ['file.driver.js', true],
+    ['file.protractor.driver.js', true],
+    ['file.protractor.uni.driver.js', true],
+    ['file.puppeteer.unidriver.private.driver.js', false],
+    ['file.private.driver.js', false],
+    ['file.private.uni.driver.js', false],
+    ['index.js', false],
+    ['file.js', false],
+    ['driver.js', false],
+  ].map(([assertion, expecation]) => {
+    it(`should return ${expecation} for ${assertion}`, () => {
       expect(isDriver(assertion)).toEqual(expecation);
     });
   });
